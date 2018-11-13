92fe2ef15faf1bd43e76ca10a699e31469b008a3
Mohamed khidir ageeb <ageeb1982@gmail.com>
2018-11-13 15:10:11 +0300

    New Add

---
+++ WebApp_Test/Controllers/articlesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+using WebApp_Test.Models;
+
+namespace WebApp_Test.Controllers
+{
+
+    [Authorize]
+    public class articlesController : Controller
+    {
+        private DB db = new DB();
+
+    
+        public ActionResult Index()
+        {
+            return View(db.articles.ToList());
+        }
+
+         public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            article article = db.articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+         public ActionResult Create()
+        {
+            return View();
+        }
+
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(article article)
+        {
+            if (ModelState.IsValid)
+            {
+                article.AddTime = DateTime.Now;
+                db.articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+         public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            article article = db.articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(article article)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                
+
+
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+         public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            article article = db.articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+         [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            article article = db.articles.Find(id);
+            db.articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+		 private bool IsExist(long id)
+        {
+            return db.articles.Any(e => e.Id == id);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApp_Test/Models/article.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_Test.Models
+{
+    public class article
+    {
+ 
+         [HiddenInput(DisplayValue = false)]
+         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 Id { get; set; }
+
+
+
+        [Display(Name = "Article Name")]
+        [RegularExpression(@"^[ (\p{L})?(\p{M})?-]*$", ErrorMessage = "Please Use letters only")]
+        [Required(ErrorMessage = "Please Input Article Name")]
+        [MaxLength(125)]
+        public string Name { set; get; }
+
+        [Display(Name = "Article body")]
+        [Required(ErrorMessage = "please Input Article body ")]
+        [RegularExpression(@"^[ \n\r(\p{L})?(\p{M})?-]*$", ErrorMessage = "Please Use letters only")]
+        [MaxLength(1000)]
+        public string Body { set; get; }
+
+
+        [Display(Name = "Article Time")]
+
+        public DateTime AddTime { set; get; }
+
+
+    }
+}
